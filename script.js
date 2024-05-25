538c84ba898e55b7505d3d0ecad7291dd7859a28
essram <esarama22@gmail.com>
2024-05-25 20:43:49 +0700

    Done

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let angka_1, angka_2, hasil;
 document.getElementById("button_jumlah").onclick = function () {
   angka_1 = document.getElementById("angka_1").value;
   console.log(`angka_1 = ${angka_1} tipe data angka_1 = ${typeof angka_1}`);
-  angka_2 = document.getElementById("angka_2").valueAsNumber;
+  
+  angka_2 = document.getElementById("angka_2").value;
   console.log(`angka_2 = ${angka_2} tipe data angka_2 = ${typeof angka_2}`);
 
-  hasil = parseFloat(angka_1) + angka_2;
+  // Konversi kedua input menjadi angka menggunakan parseFloat
+  hasil = parseFloat(angka_1) + parseFloat(angka_2);
 
   console.log(`hasil = ${hasil}  tipe = ${typeof hasil}`);
 
